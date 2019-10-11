c9ef299614bdc7393d74ba27a1e6bef976f5bda4
Martin <martinb@lagash.com>
2019-10-11 18:57:19 -0300

    agregado login

---
+++ RoyalAcademy/src/main/java/controladores/LoginControlador.java
@@ -0,0 +1,29 @@
+package controladores;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import abm.CursadaABM;
+import datos.Cursada;
+
+@Controller
+@RequestMapping(path="/Login")
+public class LoginControlador {
+	
+	
+	@GetMapping()
+	public String alta() {
+		
+		return "login";
+	}
+	
+
+}
